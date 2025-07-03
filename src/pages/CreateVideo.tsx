4acc107e428b20fef75543ed61fdd0f504e40305
Sruthi Lingam <sruthi.lingam1202@gmail.com>
2025-07-03 15:43:21 -0400

    right alignment test

--- src/pages/CreateVideo.tsx
+++ src/pages/CreateVideo.tsx
@@ -19,7 +19,7 @@ export default function CreateVideo() {
     return (
         <div style={{
             padding: '2rem',
-            textAlign: 'center',
+            textAlign: 'right', //change to right later
             color: 'white',
             minHeight: '100vh',
             backgroundColor: '#111',
